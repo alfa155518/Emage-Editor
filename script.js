a7c58ac5b9070672202b1896d93a01596f78c4ce
Ahmed Hassop <alfaelkanas@gmail.com>
2023-07-22 17:51:49 +0300

    added All Files

---
+++ script.js
@@ -0,0 +1,79 @@
+// All Variables
+
+let btnUpload = document.querySelector(".btn-upload");
+
+let allInputs = document.querySelectorAll(".item input");
+
+let imgUploaded = document.querySelector(".img-uploaded");
+
+let resetBtn = document.querySelector(".btn-reset");
+
+let inputImg = document.querySelector(".img-and-btn input");
+
+let sectionImg = document.querySelector(".img-and-btn");
+
+let btnDownload = document.querySelector(".download");
+
+let canvasImg = document.getElementById("canvas-img");
+
+let theContext = canvasImg.getContext("2d");
+
+// btn Upload Image
+btnUpload.addEventListener("click", handel);
+
+// function Upload Image
+function handel() {
+  imgUploaded.classList.add("active");
+
+  resetBtn.classList.add("active");
+
+  btnDownload.classList.add("active");
+
+  const fr = new FileReader();
+  fr.addEventListener("load", (e) => {
+    imgUploaded.src = fr.result;
+    imgUploaded.onload = function () {
+      canvasImg.width = imgUploaded.width;
+      canvasImg.height = imgUploaded.height;
+      theContext.drawImage(
+        imgUploaded,
+        0,
+        0,
+        canvasImg.width,
+        canvasImg.height
+      );
+      imgUploaded.style.display = "none";
+    };
+  });
+  fr.readAsDataURL(inputImg.files[0]);
+}
+
+// set Filters
+allInputs.forEach((input) => {
+  input.addEventListener("change", handelUpdate);
+});
+
+// function Set Filters
+function handelUpdate() {
+  const suffix = this.dataset.sizing || "";
+  theContext.filter = `${this.id}(${this.value + suffix})`;
+  theContext.drawImage(imgUploaded, 0, 0, canvasImg.width, canvasImg.height);
+}
+
+// Reset All Filters And Inputs
+resetBtn.addEventListener("click", handelResetInputs);
+
+// function Reset All Filters And Inputs
+function handelResetInputs() {
+  allInputs.forEach((input) => {
+    input.value = 0;
+    const suffix = input.dataset.sizing || "";
+    theContext.filter = `${input.id}(${input.value + suffix})`;
+    theContext.drawImage(imgUploaded, 0, 0, canvasImg.width, canvasImg.height);
+  });
+}
+
+// btn Download Image
+btnDownload.onclick = function () {
+  btnDownload.href = canvasImg.toDataURL("image/jpeg");
+};
